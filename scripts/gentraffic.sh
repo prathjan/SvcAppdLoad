1e3e43c0ad1f41b6225018df357d2cf5967eafac
prathjan <prathjan@cisco.com>
2021-09-10 14:50:23 -0700

    ip/port

--- scripts/gentraffic.sh
+++ scripts/gentraffic.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
+echo "ip is $1"
+echo "port is $2"
 apt install openjdk-8-jdk -y
 cd /tmp
 tar xvf apache-jmeter-5.4.1.tgz 
 #cd apache-jmeter-5.4.1/bin
 #cp /tmp/teastore_browse_nogui.jmx .
-java -jar ./apache-jmeter-5.4.1/bin/ApacheJMeter.jar -t teastore_browse_nogui.jmx -Jhostname 10.88.168.212 -Jport 8085 -JnumUser 10 -JrampUp 1 -l mylogfile.log -n
+java -jar ./apache-jmeter-5.4.1/bin/ApacheJMeter.jar -t teastore_browse_nogui.jmx -Jhostname $1 -Jport $2 -JnumUser 10 -JrampUp 1 -l mylogfile.log -n
 
 
 
